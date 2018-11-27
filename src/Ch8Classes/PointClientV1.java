7b0bfaff2142867c906a91220bcb5ffd848d9a1f
radmandesh <radmandesh@gmail.com>
2018-11-27 13:38:51 -0500

    made more stuff in ch8classes

--- src/Ch8Classes/PointClientV1.java
+++ src/Ch8Classes/PointClientV1.java
@@ -2,10 +2,19 @@ package Ch8Classes;
 
 public class PointClientV1 {
     public static void main(String[] args){
-        PointV1 point = new PointV1();
+        PointV1 point = new PointV1(0,0);
+        PointV1 point2 = new PointV1(0,0);
         System.out.println(point.x + " " + point.y);
-        point.x = 1;
-        point.y = 6;
+        System.out.println(point2.x + " " + point2.y);
+        point.translate(2,2);
+        point2.translate(3,-4);
         System.out.println(point.x + " " + point.y);
+        System.out.println(point2.x + " " + point2.y);
+        point.setLocation(4,2);
+        point2.setLocation(8,27);
+        System.out.println(point.x + " " + point.y);
+        System.out.println(point2.x + " " + point2.y);
+        System.out.println(point.distance(point2));
+        System.out.println(point.distanceFromOrigin());
     }
 }
