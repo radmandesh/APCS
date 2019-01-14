bce353eb6044ee39f01c4143e2d4c0236d1589c0
radmandesh <radmandesh@gmail.com>
2019-01-14 13:37:42 -0500

    started ch10

---
+++ src/Ch10ArrayLists/IntroToArrayLists.java
@@ -0,0 +1,13 @@
+package Ch10ArrayLists;
+
+import java.util.*;
+
+public class IntroToArrayLists {
+    public static void main(String[] args){
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Rohan Deshpande");
+        names.add("sfbdsfkj sdkjjsf");
+        names.add("alalalalallalasdlsdfksdjkfdsjhfsdoiweowefbh");
+        System.out.println(names.toString());
+    }
+}
