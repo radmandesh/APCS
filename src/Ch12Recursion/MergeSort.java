e86d3a9654bc93be7a7f2408d05029fbb40c149d
radmandesh <radmandesh@gmail.com>
2019-04-12 14:02:43 -0400

    mergesrt

--- src/Ch12Recursion/MergeSort.java
+++ src/Ch12Recursion/MergeSort.java
@@ -4,34 +4,38 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args){
-        int[] nums = {1, 6, 8, 3, 4, 2};
+        int[] nums = {1, 7, 3, 8, 4, 1, 7, 3};
         int[] nums1 = Arrays.copyOfRange(nums, 0, nums.length/2);
         int[] nums2 = Arrays.copyOfRange(nums, nums1.length, nums.length);
-        nums = merge(nums1, nums2);
+        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums2));
+        System.out.println(mergeSort(nums)) ;
         for(int i = 0; i < nums.length; i++){
-            System.out.print(nums[i] + ", ");
+            System.out.print(nums[i] + " ");
         }
     }
     public static int[] merge(int[] arr1, int[] arr2){
-        int i1 = 0;
-        int i2 = 0;
+        int i1 = 0, i2 = 0;
         int[] result = new int[arr1.length+arr2.length];
-        int i = 0;
-        while(i1 != arr1.length && i1 != arr2.length){
-            if(i1 > i2){
+        for(int i = 0; i < result.length; i++){
+            if(i2 >= arr2.length || (i1 < arr1.length && arr1[i1] <= arr2[i2])){
                 result[i] = arr1[i1];
                 i1++;
             }
-            else if(i1 < i2){
+            else {
                 result[i] = arr2[i2];
                 i2++;
-            }
-            else{
-                result[i] = arr1[i1];
-                i1++;
             }
         }
         return result;
     }
 
+    public static int[] mergeSort(int[] arr){
+        int[] nums1 = Arrays.copyOfRange(arr, 0, arr.length/2);
+        int[] nums2 = Arrays.copyOfRange(arr, nums1.length, arr.length);
+        mergeSort(nums1);
+        mergeSort(nums2);
+        merge(nums1, nums2);
+
+    }
 }
